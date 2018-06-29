bec9cba5bb2cf12978ab0ca6dccd12c1acf51865
Копранов Илья <i_kopranov@smarteng.ru>
2018-06-29 19:37:58 +0300

    initial commit

---
+++ src/main/java/Springframework_L02/Springframework_L02/App.java
@@ -0,0 +1,13 @@
+package Springframework_L02.Springframework_L02;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
